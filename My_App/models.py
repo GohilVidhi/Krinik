a6044d462f099ed44a6b5cda71d0a1175281179c
GohilVidhi <gohilvidhi3@gmail.com>
2024-06-18 15:14:38 +0530

    first commit

---
+++ My_App/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class League(models.Model):
+    league_name=models.CharField(max_length=50,blank=True,null=True)
+    short_league_name=models.CharField(max_length=50,blank=True,null=True)
+    start_league_date=models.DateTimeField()
+    end_league_date=models.DateTimeField()
+    league_image=models.ImageField(upload_to="league_image_media")
+    
+    
+    def __str__(self):
+        return self.league_name
